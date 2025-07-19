fd41d9d8ce230dc1be17b1cb3b7d220157c0b5d9
sjd-23 <SJD23Dev@proton.me>
2025-07-19 14:26:18 -0500

    Rename FirstScreen.java to GameScreen.java and remove unnecessary comments

---
+++ core/src/main/java/dev/sjd23/dungeon/GameScreen.java
@@ -0,0 +1,34 @@
+package dev.sjd23.dungeon;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        if (width <= 0 || height <= 0) return;
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
