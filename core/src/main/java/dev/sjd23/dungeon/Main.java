79c41b5f2786b7bd0580e87802a29812f253dcca
sjd-23 <SJD23Dev@proton.me>
2025-07-19 14:26:54 -0500

    Change setScreen to use GameScreen

--- core/src/main/java/dev/sjd23/dungeon/Main.java
+++ core/src/main/java/dev/sjd23/dungeon/Main.java
@@ -2,10 +2,9 @@ package dev.sjd23.dungeon;
 
 import com.badlogic.gdx.Game;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new GameScreen());
     }
 }
